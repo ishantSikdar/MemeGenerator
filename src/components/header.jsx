60e2310bc891f5b828fdd6e69b30b78ebb9954e5
Ishant <113770688+ishantSikdar@users.noreply.github.com>
2023-12-30 16:45:49 +0530

    Update header.jsx
    Removed Line React Course - 3

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,6 @@ function header() {
                     <img src="/trollface.png" alt="trollface" className="h-10" />
                     <h1>Meme Generator</h1>
                 </div>
-                <p>React Course - Project 3</p>
             </div>
         </>
     )
