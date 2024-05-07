7c2d52fcad37dd6f0a01926ae0d813a2b8a527f9
ishantSikdar <ishant9715@gmail.com>
2024-05-07 21:21:00 +0530

    Update 2.0

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,11 @@
 function header() {
     return (
-        <>
-            <div className="bg-gradient-to-r from-[#672280] to-[#A626D3] px-10 h-[100px] flex justify-between items-center text-2xl font-bold text-white">
-                <div className="flex items-center gap-5">
-                    <img src="/trollface.png" alt="trollface" className="h-10" />
-                    <h1>Meme Generator</h1>
-                </div>
+        <div className="fixed z-20 top-0 w-full bg-gradient-to-r from-[#672280] to-[#A626D3] h-[100px] flex items-center justify-center  text-2xl font-bold text-white">
+            <div className="flex items-center gap-5">
+                <img src="/trollface.png" alt="trollface" className="h-10" />
+                <h1>Meme Generator</h1>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,84 +1,42 @@
 import { useState, useEffect } from "react";
 
-function heroSection() {
+function HeroSection() {
+    const [counter, setCounter] = useState(0);
+    const [randomeMeme, setRandomMeme] = useState({});
 
-    const [allMemeImages, setAllMemeImages] = useState([]);
-    
-    
     useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
+        fetch("https://meme-api.com/gimme")
             .then(res => res.json())
-            .then(data => setAllMemeImages(data))
-    }, [])
+            .then(data => setRandomMeme(data));
+    }, [counter])
 
-    const [meme, setMeme] = useState({
-        topText: "",
-        bottomText: "",
-        memeImage: ""
-    })
-
-    function getMemeImage() {
-        const memesArray = allMemeImages.data.memes;
-        const randomMeme = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randomMeme].url;
-
-        setMeme((prevMeme) => {
-            return {
-                ...prevMeme,
-                memeImage: url
-            }
-        })
-        console.log(memesArray[randomMeme]);
-    }
+    console.log(randomeMeme)
 
     function handleChange(event) {
-        const { name, value } = event.target;
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            [name]: value
-        }))
+        setCounter(count => count + 1)
     }
 
     return (
-        <>
-            <div className="mx-auto my-5 w-2/4">
-                <div className="w-full">
-                    <div className="flex gap-5">
-                        <input className="border-black border-[1pt] outline-none h-12 p-2 w-1/2" type="text" name="topText" id="" placeholder="Top text" onChange={handleChange} />
-                        <input className="border-black border-[1pt] outline-none h-12 p-2 w-1/2" type="text" name="bottomText" id="" placeholder="Bottom text" onChange={handleChange} />
-                    </div>
-
-                    <div className="flex justify-center text-white font-bold text-xl ">
-                        <button
-                            onClick={getMemeImage}
-                            type="submit" name="submit"
-                            className="my-4 w-full p-3 shadow-xl rounded-md bg-gradient-to-r from-[#672280] to-[#A626D3] cursor-pointer"
-                        >Get a new Meme Image 🖼️</button>
-                    </div>
-
-                    <div className="w-full h-96 shadow-xl relative" style={{
-                        backgroundImage: `url(${meme.memeImage})`,
-                        backgroundSize: 'contain',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat'
-                    }}>
-                        <h1 className="absolute w-full text-center top-0 text-white font-bold text-2xl p-4" style={{
-                            textShadow: '1px 1px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black'
-                        }}>
-                            {meme.topText}
-                        </h1>
-                        <h1 className="absolute w-full text-center bottom-0 text-white font-bold text-2xl p-4" style={{
-                            textShadow: '1px 1px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black'
-                        }}>
-                            {meme.bottomText}
-                        </h1>
-                    </div>
+        <div className="flex flex-col justify-center items-center  px-5 h-screen">
+            <div className="w-full">
+                <div className="w-full h-96 shadow-xl relative" style={{
+                    backgroundImage: `url(${randomeMeme.url})`,
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat'
+                }}></div>
+                <div className="flex justify-center text-white font-bold text-xl ">
+                    <button
+                        onClick={handleChange}
+                        type="submit" name="submit"
+                        className="my-4 w-full p-3 shadow-xl rounded-md bg-gradient-to-r from-[#672280] to-[#A626D3] cursor-pointer"
+                    >Get a new Meme Image 🖼️</button>
                 </div>
             </div>
-        </>
+        </div>
     )
 
 }
 
 
-export default heroSection;
+export default HeroSection;
